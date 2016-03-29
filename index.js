9fb02c5b836d61892f223c135e1d854b9f27821a
cristobal-io <me@cristobal.io>
2016-03-29 20:53:18 +0200

    exposing at index.js properly the generated files.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 "use strict";
 
 module.exports = {
+  airline_destinations: require("./data/airline_destinations.json"),
+  airlines: require("./data/airlines.json"),
+  airport_airlines: require("./data/airport_airlines.json"),
+  airport_cities: require("./data/airport_cities.json"),
+  airport_runways: require("./data/airport_runways.json"),
   airports: require("./data/airports.json"),
   cities: require("./data/cities.json"),
-  airport_cities: require("./data/airport_cities.json"),
-  airline_destinations: require("./data/airline_destinations.json")
+  city_airports: require("./data/city_airports.json")
+
 };
