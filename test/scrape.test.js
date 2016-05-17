9fe80014980c8e34be88997ef12d9759aed11a27
cristobal-io <me@cristobal.io>
2016-05-17 09:27:56 +0200

    linting tests.

--- test/scrape.test.js
+++ test/scrape.test.js
@@ -24,40 +24,42 @@ var airlinesRaw = require("../tmp/airlines_data.json");
 
 describe("bin/scrape.js tests", function () {
 
-  describe("getAirportAirlines fn", function() {
-    it("should be a function", function() {
+  describe("getAirportAirlines fn", function () {
+    it("should be a function", function () {
       assert.ok(typeof getAirportAirlines === "function", "it's not a function");
     });
 
-    it("should return an object", function() {
+    it("should return an object", function () {
       var objectSchema = {
         "type": "object",
         "minProperties": 1
       };
-
       var airportAirlines = getAirportAirlines(destinationsRaw);
       var validateAirportAirlinesSchema = ajv.compile(objectSchema);
       var validAirportAirlines = validateAirportAirlinesSchema(airportAirlines);
 
-      assert.ok(validAirportAirlines, "doesn't meet the proper schema: " + 
-          _.get(validateAirportAirlinesSchema, "errors[0].message")
-        );
-      _.map(airportAirlines, function(airport) {
+      assert.ok(validAirportAirlines, "doesn't meet the proper schema: " +
+        _.get(validateAirportAirlinesSchema, "errors[0].message")
+      );
+      _.map(airportAirlines, function (airport) {
         assert.ok(!detectDuplicatesInArray(airport), "found duplicated values inside array");
       });
-
     });
+
   });
-  describe("helper fn", function() {
-    
-    it("has to detect duplicates", function() {
-      var arrayDuplicates = ["hi","hello","bye", "hi"];
-      var arrayWithoutDuplicates = ["hi","hello","bye"];
+
+  describe("helper fn", function () {
+
+    it("has to detect duplicates", function () {
+      var arrayDuplicates = ["hi", "hello", "bye", "hi"];
+      var arrayWithoutDuplicates = ["hi", "hello", "bye"];
 
       assert.ok(detectDuplicatesInArray(arrayDuplicates), "it is not detecting the duplicated values");
       assert.ok(!detectDuplicatesInArray(arrayWithoutDuplicates), "it is giving false positive");
     });
+
   });
+
 
   describe("getCityAirports fn", function () {
 
@@ -84,12 +86,13 @@ describe("bin/scrape.js tests", function () {
 
         assert.ok(validAirportSchema, "the airport doesn't meet the schema: " +
           _.get(validateAirportSchema, "errors[0].message"));
-        airport.map(function(value, index, collection) {
-          collection.splice(index,1);
-          assert.ok(findCity(collection,value) === -1, "there are duplicated airports");
+        airport.map(function (value, index, collection) {
+          collection.splice(index, 1);
+          assert.ok(findCity(collection, value) === -1, "there are duplicated airports");
         });
       });
     });
+
   });
 
 
@@ -103,6 +106,7 @@ describe("bin/scrape.js tests", function () {
         assert.ok(airport.name, "there is no name");
       });
     });
+
   });
 
 
@@ -125,7 +129,6 @@ describe("bin/scrape.js tests", function () {
           _.map(destinations, function (destination) {
             assert(!(/\/wiki\//.test(destination)), "the destination url contains wiki.");
           });
-
         });
       });
     });
@@ -136,14 +139,14 @@ describe("bin/scrape.js tests", function () {
 
       assert(validDestination, _.get(validateDestinationsSchema, "errors[0].message"));
     });
+
   });
 
 
   describe("reduceAirports fn", function () {
 
-    it("should meet the schema", function () {
 
-      var airports = reduceAirports(airportsRaw);
+    it("should meet the schema", function () {
       var airportSchema = require("../schema/airport.schema.json");
 
       assert(airports, "airports doesn't exist");
@@ -151,7 +154,6 @@ describe("bin/scrape.js tests", function () {
       _.map(airports, function (airport) {
         var validateAirport = ajv.compile(airportSchema);
         var validAirport = validateAirport(airport);
-
 
         assert(validAirport, JSON.stringify(validateAirport, null, 2));
       });
@@ -171,6 +173,7 @@ describe("bin/scrape.js tests", function () {
 
 
   describe("getAirportRunways fn", function () {
+
     it("should be a fn", function () {
       assert.ok(typeof getAirportRunways === "function", "this is not a fn");
     });
@@ -183,6 +186,7 @@ describe("bin/scrape.js tests", function () {
         assert.ok(runway.length > 0, "there are no runway");
       });
     });
+
   });
 
   describe("reduceAirlines fn", function () {
@@ -191,8 +195,8 @@ describe("bin/scrape.js tests", function () {
     before(function () {
       airlines = reduceAirlines(airlinesRaw);
     });
-    it("should have all a name so we can use it as a primary key", function () {
 
+    it("should have all a name so we can use it as a primary key", function () {
       _.map(airlinesRaw, function (airline) {
         assert.ok(airline.name, "doesn't has name" + airline.name);
       });
@@ -219,6 +223,7 @@ describe("bin/scrape.js tests", function () {
 
 
   describe("getIcaoName fn", function () {
+
     it("should be a function", function () {
       assert(typeof getIcaoName === "function");
     });
@@ -229,16 +234,19 @@ describe("bin/scrape.js tests", function () {
 
       assert.equal(icaoAirport, expectedIcao, "the url is not being find.");
     });
+
   });
 
 });
 
 function detectDuplicatesInArray(collection) {
-  var duplicatesFound= false;
+  var duplicatesFound = false;
 
-  _.map(collection, function(value, index, col) {
-    if (duplicatesFound) {return;}
-    col.splice(index,index+1);
+  _.map(collection, function (value, index, col) {
+    if (duplicatesFound) {
+      return;
+    }
+    col.splice(index, index + 1);
     duplicatesFound = _.includes(col, value);
   });
   return duplicatesFound;
